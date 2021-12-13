2a8397af1da2a5815145281130712a8e0de37246
Klim Klimentov <klimentov@i.ua>
2021-12-14 01:44:47 +0200

    added icons style

--- src/components/buttons/font-awesome/font-awesome.js
+++ src/components/buttons/font-awesome/font-awesome.js
@@ -2,21 +2,22 @@ import React from "react";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faFacebook, faInstagram} from "@fortawesome/free-brands-svg-icons";
 import {faLongArrowAltDown, faLongArrowAltRight, faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import './font-awesome.scss'
 
 const FaFacebook =()=>{
-    return  <FontAwesomeIcon icon={faFacebook}/>
+    return  <FontAwesomeIcon icon={faFacebook} className='colorIcon'/>
 }
 const FaInstagram =()=>{
-    return  <FontAwesomeIcon icon={faInstagram}/>
+    return  <FontAwesomeIcon icon={faInstagram} className='colorIcon'/>
 }
 const FaUserCircle =()=>{
-    return  <FontAwesomeIcon icon={faUserCircle}/>
+    return  <FontAwesomeIcon icon={faUserCircle} className='colorIcon'/>
 }
 const FaLongArrowAltDown =()=>{
-    return  <FontAwesomeIcon icon={faLongArrowAltDown}/>
+    return  <FontAwesomeIcon icon={faLongArrowAltDown} className='colorIcon'/>
 }
 const FaLongArrowAltRight =()=>{
-    return  <FontAwesomeIcon icon={faLongArrowAltRight}/>
+    return  <FontAwesomeIcon icon={faLongArrowAltRight} className='colorIcon'/>
 }
 export {
     FaFacebook,
