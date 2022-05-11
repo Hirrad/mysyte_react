ac215e58fddaa36b6b0157595f48eef730bb2889
Hirrad <hirad@i.ua>
2022-05-11 20:21:19 +0300

    added a hook to work with axios

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,43 @@
+import { useState, useEffect,useCallback} from "react";
+import axios from "axios";
+
+const useFetch = url =>{
+    const _baseUrl='http://localhost:1337/api';
+    const [response, setResponse] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [option, setOption] = useState({})
+
+    const doFetch = useCallback((option = {}) => {
+        setOption(option);
+        setIsLoading(true);
+    },[])
+    useEffect(() => {
+        const requestOptions ={
+            ...option,
+            ...{
+                headers:{
+                    // authorization:token?`Token ${token}`:''
+                }
+            }
+        }
+        if (!isLoading) return
+        axios({
+            ...requestOptions,
+            url: _baseUrl + url
+
+        })
+            .then(res => {
+                // console.log('success', res);
+                setResponse(res)
+                setIsLoading(false);
+            }).catch(error => {
+            // console.log('error', error);
+            setIsLoading(false);
+            setError(error.response.data.errors)
+        })
+    },[isLoading,option,url,token])
+
+    return [{response, isLoading, error}, doFetch]
+}
+export default useFetch;
