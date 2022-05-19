1371bd3110119a8f95ad8306b2ad9914c33b3b30
Hirrad <hirad@i.ua>
2022-05-20 00:06:15 +0300

    made a function instead of a class

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,43 +1,60 @@
-import React, {Component} from "react";
-// import {FaUserCircle, FaFacebook, FaInstagram} from "../buttons/font-awesome";
-// import {slide as Menu} from 'react-burger-menu'
-// import {Link} from "react-router-dom";
-
+import React, {useState, useEffect} from "react";
 import './navigation.scss'
 import NavigationPc from "./navigation-pc";
 import NavigationBurger from "./navigation-burger";
 
-class Navigation extends Component {
-    state = {
-        mobile: document.documentElement.clientWidth
+const Navigation =()=> {
+    const [mobile, setMobile]=useState(document.documentElement.clientWidth)
+    console.log(mobile)
+    const widthScreen= ()=>{
+        setMobile(document.documentElement.clientWidth)
     }
 
-
-
-widthScreen= (e)=> {
-        if(document.documentElement.clientWidth!==this.state.mobile)
-        {
-            let mobile=document.documentElement.clientWidth;
-            this.setState({mobile})
-        }
-
-}
-componentDidMount() {
-        window.addEventListener("resize",this.widthScreen)
-
-}
-componentWillUnmount() {
-    window.removeEventListener("resize",this.widthScreen);
-}
-
-
-    render() {
-       let {mobile}=this.state;
-       // console.log(mobile);
-        return (mobile<=980)?<NavigationBurger/>:<NavigationPc/>
-
-    }
+useEffect(() => {
+   return window.addEventListener("resize",widthScreen)
+    
+},[mobile])
+       return (mobile<=980)?<NavigationBurger/>:<NavigationPc/>   
 }
 
 
 export default Navigation;
+
+
+
+
+
+// class Navigation extends Component {
+//     state = {
+//         mobile: document.documentElement.clientWidth
+//     }
+
+
+
+// widthScreen= (e)=> {
+//         if(document.documentElement.clientWidth!==this.state.mobile)
+//         {
+//             let mobile=document.documentElement.clientWidth;
+//             this.setState({mobile})
+//         }
+
+// }
+// componentDidMount() {
+//         window.addEventListener("resize",this.widthScreen)
+
+// }
+// componentWillUnmount() {
+//     window.removeEventListener("resize",this.widthScreen);
+// }
+
+
+//     render() {
+//        let {mobile}=this.state;
+//        // console.log(mobile);
+//         return (mobile<=980)?<NavigationBurger/>:<NavigationPc/>
+
+//     }
+// }
+
+
+// export default Navigation;
