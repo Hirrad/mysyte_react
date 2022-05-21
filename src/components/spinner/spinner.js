027b2d567b51cbd924c1f0b90a3d3b9d6d64c296
Hirrad <hirad@i.ua>
2022-05-22 00:14:56 +0300

    reacted after the update

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,15 +1,24 @@
 import React from "react";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Loader from "react-loader-spinner";
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import {Bars} from 'react-loader-spinner'
+// import Loader from "react-loader-spinner";
 
-import './spinner.css'
+// import './spinner.css'
 const Spinner =() =>{
-    return <Loader
-        type="Puff"
-        color="#00BFFF"
-        height={100}
-        width={100}
+    // return <Loader
+    //     type="Puff"
+    //     color="#00BFFF"
+    //     height={100}
+    //     width={100}
 
-    />
+    // />
+    return <div className='d-flex justify-content-center align-items-center vh-100'>
+        
+        <Bars heigth="100"
+     width="100"
+      color="grey"
+       ariaLabel="loading-indicator" />
+        
+    </div>
 }
 export default Spinner;
